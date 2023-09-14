f7e771684dfd5cb54fd133965ba84ee4ab21b262
steph bug <chronhubgit@gmail.com>
2023-09-14 13:42:11 +0200

    work(package): attributes

---
+++ ResolvedListener.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Storm\Tracker;
+
+use Storm\Contract\Tracker\Listener;
+
+/**
+ * @internal
+ *
+ * @property-read object $subscriber
+ */
+final readonly class ResolvedListener implements Listener
+{
+    public function __construct(
+        public object $subscriber,
+        private string $name,
+        private int $priority
+    ) {
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function priority(): int
+    {
+        return $this->priority;
+    }
+
+    public function story(): callable
+    {
+        return ($this->subscriber)();
+    }
+
+    public function origin(): ?string
+    {
+        return $this->subscriber::class;
+    }
+}
